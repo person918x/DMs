124ceee7613b6ed95e5dcf292c91b87b5e6c7134
DED8IRD <vit5xvzqp@mozmail.com>
2025-12-06 16:06:54 -0800

    Write timestamp grouped msgs to json

--- scripts/parser.py
+++ scripts/parser.py
@@ -4,12 +4,12 @@ Parse raw messages into CSV of messages grouped by timestamp
 import json
 import re
 
-HEADERS = ['time', 'message']
+HEADERS = ['time', 'messages']
 TIMESTAMP_PATTERN = r'(\[\w+ \d+:\d+ \w+\])'
 
 if __name__ == '__main__':
   message_groups = []
-  output = {}
+  output = []
   with open('raw.txt', 'r', encoding='utf-8') as f:
     content = f.read()
     message_groups = re.split(TIMESTAMP_PATTERN, content)
@@ -17,14 +17,17 @@ if __name__ == '__main__':
   msg_group = []
   for text in message_groups:
     if re.match(TIMESTAMP_PATTERN, text):
-      output[text] = msg_group
+      output.append({ 'time': text, 'messages': msg_group })
       msg_group = []
     else:
+      # Consider new paragraphs as individual texts
+      # Ignore whitespace only lines leading/trailing whitespace
       messages = text.split('\n\t')
       for msg in messages:
         if len(msg.split()):
           msg_group.append(msg.strip())
-  # epilogue
-  output['EPILOGUE'] = msg_group
+  # Epilogue
+  output.append({ 'time': 'Now', 'messages': msg_group })
 
-  print(json.dumps(output, indent=2))
+  with open('../src/assets/messages.json', 'w') as f:
+    json.dump(output, f, indent=2)
