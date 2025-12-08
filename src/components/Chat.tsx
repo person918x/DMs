395f91dc10171a184949ae768b647f5d72ea981a
DED8IRD <vit5xvzqp@mozmail.com>
2025-12-08 15:15:09 -0800

    Add local avatar

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -4,11 +4,12 @@ import MessageField from "./MessageField";
 import message_groups from '../assets/messages.json';
 import ally_mcbeal_screenplay from '../assets/ally-mcbeal.txt';
 import felicity_screenplay from '../assets/felicity.txt';
+import avatar from '../assets/918-avatar.jpg';
 
 export default function Chat() {
   const contact = {
     name: 'Do Not Answer',
-    avatar: "https://scontent-sea5-1.xx.fbcdn.net/v/t39.30808-1/462140760_2807746069407758_5993805066772312114_n.jpg?stp=c161.0.966.966a_dst-jpg_s200x200_tt6&_nc_cat=110&ccb=1-7&_nc_sid=f907e8&_nc_ohc=XeeGZM0okfMQ7kNvwGQiCqw&_nc_oc=Adn8jhPOEkaWMnPk7QqXpHIC8HzeUVeLvcjvmueUlK481Na5H02XAwZri8tqH8ERUIY&_nc_zt=24&_nc_ht=scontent-sea5-1.xx&_nc_gid=zN5LmOfIMkggztpz9eKIDg&oh=00_AfkhGVCyNreyrltpDzQ3MkF4StqK5tfQ7rnG8zxgBF-WeA&oe=69372631",
+    avatar,
   }
 
   return (
